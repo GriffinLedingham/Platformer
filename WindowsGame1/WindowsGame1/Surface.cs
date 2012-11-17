87b50f6e5b4c1d5a2f19acb8482e3f58b73be6cd
Griffin Ledingham <gcledingh@gmail.com>
2012-11-17 01:38:40 -0800

    r1 Initial commit

---
+++ WindowsGame1/WindowsGame1/Surface.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace WindowsGame1
+{
+    public class Surface
+    {   
+        public SpriteBatch spriteBatch;
+        public Texture2D SpriteTexture;
+        public Rectangle pos;
+
+        public Surface(ContentManager Content, SpriteBatch spriteBatch, int width, int height, int xStart, int yStart)
+        {
+            this.spriteBatch = spriteBatch;
+            SpriteTexture = Content.Load<Texture2D>("groundTile");
+            this.pos.Height = height;
+            this.pos.Width = width;
+            this.pos.X = xStart;
+            this.pos.Y = yStart;
+        }
+
+        public void draw()
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(SpriteTexture, pos, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
